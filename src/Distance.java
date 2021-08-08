146c6491922ec1c18aad13e5e44b0669dd6a886a
SRC0747 <srcsrk1998@gmail.com>
2021-08-08 23:02:43 +0530

    Find distance of a number from co-ordinate.

---
+++ src/Distance.java
@@ -0,0 +1,10 @@
+public class Distance {
+    public static void main(String arg[])
+    {
+        int x1,x2,y1,y2;
+        double dis;
+        x1=0;y1=0;x2=5;y2=7;
+        dis=Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+        System.out.println("distancebetween"+"("+x1+","+y1+"),"+"("+x2+","+y2+")===>"+dis);
+    }
+}
